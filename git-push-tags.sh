c2a808d51e8513979dcc0106352b91c2bc54f316
marc.pignat@hevs.ch <marc@pignat.org>
2026-01-06 08:42:32 +0100

    fix tags

--- git-push-tags.sh
+++ git-push-tags.sh
@@ -2,5 +2,5 @@
 DATE=$(date -u +'%Y-%m-%dT%H-%M-%S')
 echo -n "$DATE" > site/RELEASE
 git commit site/RELEASE -m "$DATE"
-git tag "$DATE" -m "$DATE"
+git tag -a "$DATE" -m "$DATE"
 git push --follow-tags
